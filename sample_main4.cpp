eca0a98721c6cf470c3b2861c4b71e20873a5afb
Michael Brinkman <michael11@build.tamu.edu>
2015-11-27 00:27:49 -0600

    Cleaned up a bunch of things and various work

--- sample_main4.cpp
+++ sample_main4.cpp
@@ -1,56 +1,38 @@
 #include "std_lib_facilities_4.h"
 #include "Simple_window.h"
 #include "Graph.h"
-//#include <vector>
-//#include <vector>
+#include "GUI.h"
 
 Vector_ref<Ellipse> Pancake_Maker(int level) {
-//	Vector_ref<Ellipse> pancake(9);
-	Vector_ref<Ellipse> pancake;
+	Vector_ref<Ellipse> pancakes;
 	for (int i = 0; i < level; i++) {
-		Ellipse temp(Point(300, 300-20*i), 100-3i, 20);
-		pancake.push_back(temp);
-//		pancake[i] = Ellipse(Point(300, 300-20*i), 20, 100-3*i);
-	
-//		cout << 
-//		return pancake;
+		Ellipse* temp = new Ellipse(Point(300, 300-20*i), 100-3*i, 20);
+		pancakes.push_back(temp);
 	}
-	return pancake;
+	return pancakes;
 }
 
 int main()
 try {
-  Vector_ref<Ellipse> Pancakes;
-  if(H112 != 201401L)error("Error: incorrect std_lib_facilities_4.h version ",
-		  	   H112);
-  Simple_window win1(Point(100,200),600,400,"two lines");
-  Line horizontal(Point(100,100),Point(200,100));  // make a horizontal line
+  Simple_window win1(Point(100,200),600,400, "pankake");
+	Vector_ref<Ellipse> pancakes = Pancake_Maker(8);
+	Text Splash_text(Point(200,200), "James Dean faked his own death");
+	win1.attach(Splash_text);
+	win1.wait_for_button();
+	win1.detach(Splash_text);
+	Text Rules(Point(200,200), "Rules are for nerds");
+	win1.attach(Rules);
+	win1.wait_for_button();
+	win1.detach(Rules);
+	int level = 7;
+	for(int i=0; i < level; i++){
+        	pancakes[i].set_fill_color(94);
+		win1.attach(pancakes[i]);
+  }
 
-  Pancakes = Pancake_Maker(8);
-//  Pancakes.push_back(new Ellipse(Point (300, 300), 20, 100));
+	win1.wait_for_button();
 
-//  cout << Pancakes.size() << endl;
-
-  //win1.attach(*Pancakes[0]);
-
-//  for(int i = 0; i < Pancakes.size(); i++) {
-    win1.attach((Pancakes[0]));
-//  }
-  Line vertical(Point(150,50),Point(150,150));     // make a vertical line
-//  vertical.set_style(Line_style(Line_style::solid, 1));
-  win1.attach(horizontal);
-  win1.attach(vertical);
-  vertical.set_color(Color::magenta);
-  cout << "here" << flush << endl;
-  win1.wait_for_button();
-  vector<int> my_int {0, 1, 2};
-  
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!EXAMPLE SWAP!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
-  // int temp = my_int[0];
-  //my_int[0] = my_int[2];
-  /my_int[2] = temp;
-
-  return 0;
+	return 0;
 }
 catch(exception& e) {
 	cerr << "exception: " << e.what() << '\n';

1fbe927b9653b1a99511c89699e0a055625207f4
Michael Brinkman <michael11@build.tamu.edu>
2015-11-27 22:28:07 -0600

    further improvements

--- sample_main4.cpp
+++ sample_main4.cpp
@@ -24,11 +24,15 @@ try {
 	win1.attach(Rules);
 	win1.wait_for_button();
 	win1.detach(Rules);
+	Text Level_Chooser(Point(200,200), "Choose from <insert buttons> to know how many pancakes");
+	win1.attach(Level_Chooser);
+	win1.wait_for_button();
+	win1.detach(Level_Chooser);
 	int level = 7;
-	for(int i=0; i < level; i++){
+	for(int i=0; i < level; i++) {
         	pancakes[i].set_fill_color(94);
 		win1.attach(pancakes[i]);
-  }
+  	}
 
 	win1.wait_for_button();
 
